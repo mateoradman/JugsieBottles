907468e08612d17cdf88a9b0b0d192909155b06a
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-10-22 08:47:00 +0200

    Add marquee animation to tailwind.config.js.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
       'sans': ['"Modern Era"', 'system-ui', '-apple-system', '"Segoe UI"',
         'Roboto', 'Ubuntu', 'Cantarell', '"Noto Sans"', 'sans-serif']
     },
-    extend: {},
+    extend: {
+
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': {transform: 'translateX(0%)'},
+          '100%': {transform: 'translateX(-100%)'},
+        },
+        marquee2: {
+          '0%': {transform: 'translateX(100%)'},
+          '100%': {transform: 'translateX(0%)'},
+        },
+      },
+    },
   },
   variants: {
     extend: {},
