df02fa50cdbe631c65657a8c749ec437c4a01da7
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-10-25 08:20:30 +0200

    Style hero.

--- frontend/components/hero/index.js
+++ frontend/components/hero/index.js
@@ -5,21 +5,21 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="hero min-h-screen"
+    <div className="hero h-screen"
          style={{
            backgroundImage: `url(${HeroPhoto.src})`
          }}>
       <div className="hero-overlay bg-opacity-60"/>
       <div className="text-center hero-content text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">
-            Jugsie Bottles
+          <h1 className="mb-5 text-5xl font-extrabold tracking-wider">
+            JUGSIE BOTTLES
           </h1>
           <p className="mb-5">
             Tvoja boca za svaki dan.
           </p>
           <Link href="/shop">
-            <button className="btn btn-outline">Kupi sada</button>
+            <button className="btn btn-outline text-white">Kupi sada</button>
           </Link>
         </div>
       </div>
