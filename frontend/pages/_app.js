6e02634815c993a4f95db77a85c5a140c3c807d6
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-10-28 10:44:54 +0200

    Add a standard layout for every page.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,14 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/overrideNextJSImageSettings.css'
 import '../styles/globals.css'
+import Layout from "../components/layout";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp
