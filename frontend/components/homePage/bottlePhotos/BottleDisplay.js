1f7fb8cbe8a005b97e10092bd8f6f26726af9540
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-10-29 15:16:40 +0200

    BottleDisplay styling.

--- frontend/components/homePage/bottlePhotos/BottleDisplay.js
+++ frontend/components/homePage/bottlePhotos/BottleDisplay.js
@@ -12,17 +12,18 @@ const bottles = [
 
 const BottleDisplay = () => {
   return (
-    <div className="bg-transparent py-15 md:py-20">
-      <div className="w-full items-center p-4 space-x-1 carousel">
+    <div className="bg-transparent md:pt-10 md:pb-20">
+      <div className="w-full items-center p-4 space-x-1 carousel space-x-10">
         {bottles.map((item) => (
           <div key={item.bottleName}
-               className="carousel-item w-2/7 h-60 md:h-auto md:max-h-120 relative">
+               className="carousel-item w-2/7 h-60 md:h-120 relative">
             <Image
               src={item.bottle}
               alt={item.bottleName + " Jugsie Bottle"}
               objectFit={"contain"}
               layout={"fill"}
               priority={"true"}
+              className={"bg-transparent"}
             />
           </div>
         ))}
