5765d3d98eb5da30f447449e874de3c0d5bcb1e7
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-10-29 13:26:57 +0200

    Add favicon and move Head and Script tags to Layout.

--- frontend/components/layout/index.js
+++ frontend/components/layout/index.js
@@ -1,12 +1,20 @@
 import Navbar from "../navigation";
 import Footer from "../footer";
+import Head from "next/head";
+import Script from "next/script";
 
-export default function Layout({ children }) {
+export default function Layout({children}) {
   return (
     <div className="bg-white">
-      <Navbar />
+      <Head>
+        <title>Jugsie Bottles</title>
+        <link rel="icon" type="image/svg+xml" href="/Favicon/favicon.svg"/>
+        <link rel="icon" type="image/png" href="/Favicon/favicon.png"/>
+      </Head>
+      <Script src="https://scripts.sirv.com/sirv.js"/>
+      <Navbar/>
       <main>{children}</main>
-      <Footer />
+      <Footer/>
     </div>
   )
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,35 +1,26 @@
-import Script from 'next/script';
-import Head from 'next/head';
 import Hero from "../components/homePage/hero";
 import BottleDisplay from "../components/homePage/bottlePhotos/BottleDisplay";
 import MarqueeRunningText from "../components/homePage/runningText";
 import ThreeSixtyCard from "../components/homePage/cards/360Card";
 import HomePageCard from "../components/homePage/cards/HomePageCard";
-import CardPhotoOne from "../public/ContentPhotos/IMG_9820.JPG"
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Jugsie Bottles</title>
-      </Head>
-      <Script src="https://scripts.sirv.com/sirv.js"/>
-      <div
-        className="relative min-h-screen min-w-full overflow-hidden">
-        <Hero/>
-        <MarqueeRunningText/>
-        <BottleDisplay/>
+    <div
+      className="relative min-h-screen min-w-full overflow-hidden">
+      <Hero/>
+      <MarqueeRunningText/>
+      <BottleDisplay/>
 
-        <div
-          className="p-5 grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-8">
-          <HomePageCard
-            cardImageSrc={CardPhotoOne}
-            cardTitle={"Some Title"}
-            cardBody={"lorem ipsum something else here"}
-          />
-          <ThreeSixtyCard/>
-        </div>
+      <div
+        className="p-5 grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-8">
+        <HomePageCard
+          cardImageSrc={"/ContentPhotos/IMG_9820.JPG"}
+          cardTitle={"Some Title"}
+          cardBody={"lorem ipsum something else here"}
+        />
+        <ThreeSixtyCard/>
       </div>
-    </>
+    </div>
   )
 }
