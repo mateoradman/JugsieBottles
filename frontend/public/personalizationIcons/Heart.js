0c465bc25849c12910d6f627f0c4022d02aff685
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-11-02 22:02:56 +0100

    Convert svg engraving icons to SVGR.

---
+++ frontend/public/personalizationIcons/Heart.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+function SvgHeart(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 437.775 437.774"
+      {...props}
+    >
+      <path d="M316.722 29.761c66.852 0 121.053 54.202 121.053 121.041 0 110.478-218.893 257.212-218.893 257.212S0 266.569 0 150.801c0-83.217 54.202-121.04 121.041-121.04 40.262 0 75.827 19.745 97.841 49.976 22.017-30.231 57.588-49.976 97.84-49.976z" />
+    </svg>
+  );
+}
+
+export default SvgHeart;
