90399be34d6da2e14a4f9245abbe5faf70bf4dce
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-11-09 22:47:37 +0100

    Rename prop to bottle.

--- frontend/components/cart/CartProduct.js
+++ frontend/components/cart/CartProduct.js
@@ -4,8 +4,8 @@ const CartProduct = (props) => {
     <li className="py-6 flex">
       <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
         <img
-          src={props.product.mainPhoto}
-          alt={props.product.alt}
+          src={props.bottle.mainPhoto}
+          alt={props.bottle.alt}
           className="w-full h-full object-center object-cover"
         />
       </div>
@@ -14,14 +14,14 @@ const CartProduct = (props) => {
         <div>
           <div className="flex justify-between text-base font-medium text-gray-900">
             <h3>
-              <a href={props.product.href}>{props.product.name}</a>
+              <a href={props.bottle.href}>{props.bottle.name}</a>
             </h3>
-            <p className="ml-4">{props.product.price}</p>
+            <p className="ml-4">{props.bottle.price}</p>
           </div>
-          <p className="mt-1 text-sm text-gray-500">{props.product.color}</p>
+          <p className="mt-1 text-sm text-gray-500">{props.bottle.color}</p>
         </div>
         <div className="flex-1 flex items-end justify-between text-sm">
-          <p className="text-gray-500">Qty {props.product.quantity}</p>
+          <p className="text-gray-500">Qty {props.bottle.quantity}</p>
 
           <div className="flex">
             <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
