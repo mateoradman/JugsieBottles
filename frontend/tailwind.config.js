814b1b3295842d9efda3e84d2c020796b060fde2
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-12-05 01:33:38 +0100

    add tailwindcss forms package.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -84,6 +84,7 @@ module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
     require('daisyui'),
   ],
   daisyui: {
