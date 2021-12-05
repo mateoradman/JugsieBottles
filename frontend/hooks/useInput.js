2029b3576166ee56a219ceb67320b6e24fd2e7f6
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-12-05 18:35:17 +0100

    fix indentation in custom useInput hook.

--- frontend/hooks/useInput.js
+++ frontend/hooks/useInput.js
@@ -5,9 +5,9 @@ const useInput = (validateValue) => {
   const [isTouched, setIsTouched] = useState(false);
   const valueIsValid = validateValue(enteredValue);
   const hasError = !valueIsValid && isTouched;
-  
+
   const valueChangeHandler = (event) => {
-   setEnteredValue(event.target.value);
+    setEnteredValue(event.target.value);
   }
 
   const inputBlurHandler = (event) => {
