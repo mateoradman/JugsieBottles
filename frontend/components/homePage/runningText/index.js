ca82bd932ac673a405b22dc398b6899905ea31de
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-12-06 19:29:53 +0100

    Update marquee running text and styling.

--- frontend/components/homePage/runningText/index.js
+++ frontend/components/homePage/runningText/index.js
@@ -10,7 +10,8 @@ const MarqueeRunningText = () => {
         <RunningText/>
       </div>
 
-      <div className="mt-5 md:mt-10 absolute top-0 py-6 md:py-12 animate-marquee2">
+      <div
+        className="mt-5 md:mt-10 absolute top-0 py-6 md:py-12 animate-marquee2">
         <RunningText/>
         <RunningText/>
         <RunningText/>
@@ -23,8 +24,8 @@ const MarqueeRunningText = () => {
 const RunningText = () => {
   return (
     <span className="mx-5 whitespace-nowrap text-5xl tracking-wide md:text-9xl text-transparent
-    font-bold bg-gradient-to-r bg-clip-text from-blue-500 via-yellow-600 to-red-500">
-      48h cold 24h hot.
+    font-extrabold bg-gradient-to-r bg-clip-text from-blue-500 to-red-500">
+      24h cold 12h hot.
     </span>
   )
 }
