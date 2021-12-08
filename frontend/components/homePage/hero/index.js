b6a930e9ab34cde4553ec293132de3f51ad95e5b
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-12-08 09:02:53 +0100

    Restyle Hero component

--- frontend/components/homePage/hero/index.js
+++ frontend/components/homePage/hero/index.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import HeroPhoto from '../../../public/Hero/Hero.jpg';
+import HeroPhoto from "../../../public/Hero/Hero.jpg";
 import Link from "next/link";
-
 
 const Hero = () => {
   return (
-    <div className="hero h-120 md:h-160"
-         style={{
-           backgroundImage: `url(${HeroPhoto.src})`
-         }}>
-      <div className="hero-overlay bg-opacity-60"/>
-      <div className="text-center hero-content text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-extrabold tracking-wider">
-            JUGSIE BOTTLES
-          </h1>
-          <p className="mb-5">
-            Tvoja boca za svaki dan.
-          </p>
-          <Link href="/shop">
-            <button className="btn btn-outline text-white">Kupi sada</button>
-          </Link>
-        </div>
+    <div
+      className="hero h-120 md:h-160"
+      style={{
+        backgroundImage: `url(${HeroPhoto.src})`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-80" />
+      <div className="hero-content text-neutral-content w-full text-center flex-col">
+        <h1
+          className="-mt-8 text-5xl sm:text-6xl pb-2 sm:pb-3 md:text-8xl font-extrabold text-transparent
+          bg-gradient-to-r bg-clip-text from-blue-500 to-green-300 whitespace-nowrap tracking-tight"
+        >
+          Jugsie Bottle,
+        </h1>
+        <p className="mb-3 sm:mb-5 -mt-2 text-2xl sm:text-3xl md:text-6xl font-extrabold text-white">
+          your everyday friend.
+        </p>
+        <Link href="/shop">
+          <button className="btn btn-outline text-white text-sm md:w-1/5 sm:text-lg md:text-xl">
+            Kupi sada
+          </button>
+        </Link>
       </div>
     </div>
   );
 };
 
-
 export default Hero;
-
