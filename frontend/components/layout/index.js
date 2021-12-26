07c1f7adeee38a3d681b023d79f5e50823820606
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-12-26 18:54:50 +0100

    Update inter font css

--- frontend/components/layout/index.js
+++ frontend/components/layout/index.js
@@ -2,30 +2,32 @@ import Navbar from "../navigation";
 import Footer from "../footer";
 import Head from "next/head";
 import Script from "next/script";
-import {CartProvider} from "../../context/Context";
+import { CartProvider } from "../../context/Context";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div className="mx-auto bg-white w-full h-max min-h-full max-w-screen-2xl">
       <Head>
         <title>Jugsie Bottles</title>
-        <link rel="icon" type="image/svg+xml" href="/Favicon/favicon.svg"/>
-        <link rel="icon" type="image/png" href="/Favicon/favicon.png"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="icon" type="image/svg+xml" href="/Favicon/favicon.svg" />
+        <link rel="icon" type="image/png" href="/Favicon/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect"
-              href="https://fonts.gstatic.com"
-              crossOrigin="anonymous"/>
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-          rel="stylesheet"/>
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <Script src="https://scripts.sirv.com/sirv.js"/>
-      <Script type="text/javascript" src="https://2pay-js.2checkout.com/v1/2pay.js"/>
+      <Script src="https://scripts.sirv.com/sirv.js" />
+      <Script type="text/javascript"
+        src="https://2pay-js.2checkout.com/v1/2pay.js" />
       <CartProvider>
-        <Navbar/>
-        <main>{children}</main>
+        <Navbar />
+        <main>{ children }</main>
       </CartProvider>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
