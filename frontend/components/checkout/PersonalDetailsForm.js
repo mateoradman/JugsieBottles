7826def1279a748ea5f24f62bd647ff2f67d3d9d
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2021-12-27 13:20:44 +0100

    Fix typo in form data in PersonalDetailsForm

--- frontend/components/checkout/PersonalDetailsForm.js
+++ frontend/components/checkout/PersonalDetailsForm.js
@@ -91,7 +91,7 @@ export default function PersonalDetailsForm(props) {
         LastName: enteredLastName,
         Email: enteredEmail,
         Phone: enteredPhone,
-        Address1: enteredStreet,
+        Address: enteredStreet,
         City: enteredCity,
         Zip: enteredZIP,
         CountryCode: selectedCountry
