20089bbe98207cb11dd65bc090a895c41a03d510
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-01-02 17:15:21 +0100

    Reformatting the code

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,118 +1,117 @@
-import Cookies from 'js-cookie';
 import {
-  Aeroplane, Butterfly, Camera, Clover, Crown, Dumbbell, Flower,
-  Heart, Lipstick, MusicalNote, Paw, Smile, Star, Tooth
+  Aeroplane,
+  Butterfly,
+  Camera,
+  Clover,
+  Crown,
+  Dumbbell,
+  Flower,
+  Heart,
+  Lipstick,
+  MusicalNote,
+  Paw,
+  Smile,
+  Star,
+  Tooth,
 } from "../public/personalizationIcons";
 import { getBottlePaths } from "./general";
 
 export const reviews = {
-  href: 'https://www.trustpilot.com/review/jugsie.com',
+  href: "https://www.trustpilot.com/review/jugsie.com",
   average: 4.4,
-  totalCount: 12
-}
+  totalCount: 12,
+};
 
 export const bottleInformation = [
   {
     id: 1,
-    name: 'White',
-    class: 'bg-bottle-white',
+    name: "White",
+    class: "bg-bottle-white",
     backgroundColor: "bg-bottleBg-white",
-    mainPhoto: getBottlePaths('White'),
-    emptyPhoto: getBottlePaths('White', true)
+    mainPhoto: getBottlePaths("White"),
+    emptyPhoto: getBottlePaths("White", true),
   },
   {
     id: 2,
-    name: 'Black',
-    class: 'bg-bottle-black',
+    name: "Black",
+    class: "bg-bottle-black",
     backgroundColor: "bg-bottleBg-black",
-    mainPhoto: getBottlePaths('Black'),
-    emptyPhoto: getBottlePaths('Black', true)
+    mainPhoto: getBottlePaths("Black"),
+    emptyPhoto: getBottlePaths("Black", true),
   },
   {
     id: 3,
-    name: 'Blue',
-    class: 'bg-bottle-blue',
+    name: "Blue",
+    class: "bg-bottle-blue",
     backgroundColor: "bg-bottleBg-blue",
-    mainPhoto: getBottlePaths('Blue'),
-    emptyPhoto: getBottlePaths('Blue', true)
+    mainPhoto: getBottlePaths("Blue"),
+    emptyPhoto: getBottlePaths("Blue", true),
   },
   {
     id: 4,
-    name: 'Hot Pink',
-    class: 'bg-bottle-hotpink',
+    name: "Hot Pink",
+    class: "bg-bottle-hotpink",
     backgroundColor: "bg-bottleBg-hotpink",
-    mainPhoto: getBottlePaths('Pink'),
-    emptyPhoto: getBottlePaths('Pink', true)
+    mainPhoto: getBottlePaths("Pink"),
+    emptyPhoto: getBottlePaths("Pink", true),
   },
   {
     id: 5,
-    name: 'Lilac',
-    class: 'bg-bottle-lilac',
+    name: "Lilac",
+    class: "bg-bottle-lilac",
     backgroundColor: "bg-bottleBg-lilac",
-    mainPhoto: getBottlePaths('Lilac'),
-    emptyPhoto: getBottlePaths('Lilac', true)
+    mainPhoto: getBottlePaths("Lilac"),
+    emptyPhoto: getBottlePaths("Lilac", true),
   },
   {
     id: 6,
-    name: 'Turquoise',
-    class: 'bg-bottle-turquoise',
+    name: "Turquoise",
+    class: "bg-bottle-turquoise",
     backgroundColor: "bg-bottleBg-turquoise",
-    mainPhoto: getBottlePaths('Turquoise'),
-    emptyPhoto: getBottlePaths('Turquoise', true)
+    mainPhoto: getBottlePaths("Turquoise"),
+    emptyPhoto: getBottlePaths("Turquoise", true),
   },
   {
     id: 7,
-    name: 'Green',
-    class: 'bg-bottle-green',
+    name: "Green",
+    class: "bg-bottle-green",
     backgroundColor: "bg-bottleBg-green",
-    mainPhoto: getBottlePaths('Green'),
-    emptyPhoto: getBottlePaths('Green', true)
+    mainPhoto: getBottlePaths("Green"),
+    emptyPhoto: getBottlePaths("Green", true),
   },
-]
+];
 
 export const personalizationIcons = [
-  { name: 'aeroplane', icon: Aeroplane },
-  { name: 'butterfly', icon: Butterfly },
-  { name: 'camera', icon: Camera },
-  { name: 'clover', icon: Clover },
-  { name: 'crown', icon: Crown },
-  { name: 'dumbbell', icon: Dumbbell },
-  { name: 'flower', icon: Flower },
-  { name: 'heart', icon: Heart },
-  { name: 'lipstick', icon: Lipstick },
-  { name: 'musical-note', icon: MusicalNote },
-  { name: 'paw', icon: Paw },
-  { name: 'smile', icon: Smile },
-  { name: 'star', icon: Star },
-  { name: 'tooth', icon: Tooth },
-]
+  { name: "aeroplane", icon: Aeroplane },
+  { name: "butterfly", icon: Butterfly },
+  { name: "camera", icon: Camera },
+  { name: "clover", icon: Clover },
+  { name: "crown", icon: Crown },
+  { name: "dumbbell", icon: Dumbbell },
+  { name: "flower", icon: Flower },
+  { name: "heart", icon: Heart },
+  { name: "lipstick", icon: Lipstick },
+  { name: "musical-note", icon: MusicalNote },
+  { name: "paw", icon: Paw },
+  { name: "smile", icon: Smile },
+  { name: "star", icon: Star },
+  { name: "tooth", icon: Tooth },
+];
 
-export const LocalStorageCartItems = 'JugsieBottlesCartItems';
+export const LocalStorageCartItems = "JugsieBottlesCartItems";
 
 export const bottleFeatures = [
-  { name: 'Manufacturing country', description: 'China' },
+  { name: "Manufacturing country", description: "China" },
   {
-    name: 'Material',
-    description: 'Stainless steel'
+    name: "Material",
+    description: "Stainless steel",
   },
-  { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
-  { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
-  { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
+  { name: "Dimensions", description: '6.25" x 3.55" x 1.15"' },
+  { name: "Finish", description: "Hand sanded and finished with natural oil" },
+  { name: "Includes", description: "Wood card tray and 3 refill packs" },
   {
-    name: 'Considerations',
-    description: 'Washing the bottle in the dishwasher may cause damage to the colour coating.'
+    name: "Considerations",
+    description:
+      "Washing the bottle in the dishwasher may cause damage to the colour coating.",
   },
-]
-
-export const APIUrl = 'http://0.0.0.0:8000/api/v1/'
-
-export const APIRequestDefaults = {
-  'method': 'GET',
-  'credentials': 'include',
-  'headers': new Headers({
-    'X-CSRFToken': Cookies.get('csrftoken'),
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest'
-  }),
-};
+];
--- frontend/utils/general.js
+++ frontend/utils/general.js
@@ -1,34 +1,36 @@
-import {bottleInformation, personalizationIcons} from "./constants";
+import { bottleInformation, personalizationIcons } from "./constants";
 
 export function getBottlePaths(name, noLogo = false) {
-  const path = require('path');
+  const path = require("path");
   if (noLogo) {
-    return path.join('/bottles/withoutLogo', `${name}.png`);
+    return path.join("/bottles/withoutLogo", `${name}.png`);
   } else {
-    return path.join('/bottles', `${name}.png`);
+    return path.join("/bottles", `${name}.png`);
   }
 }
 
 export function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
-export function getFormattedPrice(price, currency = 'kn') {
+export function getFormattedPrice(price, currency = "kn") {
   return `${price} ${currency}`;
 }
 
 export function getPersonalizationIcon(iconName) {
-  let finding = personalizationIcons.find(x => x.name === iconName);
+  let finding = personalizationIcons.find((x) => x.name === iconName);
   if (finding) return finding.icon;
 }
 
 export function getBottleInformationObject(bottleName) {
-  return bottleInformation.find(x => x.name === bottleName);
+  return bottleInformation.find((x) => x.name === bottleName);
 }
 
 export function getCartTotalPrice(cartItemsArray) {
   if (cartItemsArray.length > 0) {
-    return cartItemsArray.map(item => item.price).reduce((prev, next) => prev + next);
+    return cartItemsArray
+      .map((item) => item.price)
+      .reduce((prev, next) => prev + next);
   } else return 0;
 }
 
@@ -36,5 +38,5 @@ export function ID() {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
   // after the decimal.
-  return '_' + Math.random().toString(36).substr(2, 9);
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
