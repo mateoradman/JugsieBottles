045e07ada2c6d940c446237ca0a4ca5a91ef4bbe
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-01-04 11:08:16 +0100

    Authenticate before getting static props.

--- frontend/pages/shop.js
+++ frontend/pages/shop.js
@@ -1,22 +1,25 @@
 import { ProductCard } from "../components/shop/productDetail";
 import TechnicalSpecs from "../components/shop/productTechnicalSpecs";
-import { APIRequestDefaults, APIUrl } from "../utils/constants";
+import { getAuthToken, getDefaultRequest } from "../utils/api";
 
 export default function Shop({ product }) {
   return (
     <>
-      <ProductCard product={ product } />
+      <ProductCard product={product} />
       <TechnicalSpecs />
     </>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
-  const url = new URL('product/bottles/1', APIUrl)
-  const res = await fetch(url.href, APIRequestDefaults)
-  const product = await res.json()
+  let authHeader = await getAuthToken();
+  let customRequest = getDefaultRequest();
+  customRequest.headers["Authorization"] = authHeader;
+  const url = new URL("product/bottles/1", process.env.API_HOST);
+  const res = await fetch(url.href, customRequest);
+  const product = await res.json();
 
   return {
-    props: { product }// will be passed to the page component as props
-  }
+    props: { product }, // will be passed to the page component as props
+  };
 }
