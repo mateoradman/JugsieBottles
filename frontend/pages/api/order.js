e71a2c1721b1c65d7e88dc38ab0679abb8b196bb
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-01-22 16:04:34 +0100

    Fix a typo in order.js and restructure ordering endpoint

--- frontend/pages/api/order.js
+++ frontend/pages/api/order.js
@@ -11,17 +11,24 @@ export default async function submitOrderToDjangoAPI(req, res) {
       return res.status(400).json({ error: "Bad or unauthorized request." });
     }
     let authHeader = await getAuthToken();
-    console.log("req :>> ", req);
-    console.log(`req.headers.referer`, req.headers.referer);
     const customRequestDetails = getDefaultRequest("POST");
     customRequestDetails.body = req.body;
     customRequestDetails.headers["Authorization"] = authHeader;
     customRequestDetails.headers["X-CSRFToken"] = req.cookies["csrftoken"];
     const url = new URL("shop/test/", process.env.API_HOST);
     await fetch(url.href, customRequestDetails)
-      .then((response) => response.json())
-      .then(() => res.status(200).json({ success: "Order sucessfully submitted." }))
-      .catch(() => res.status(500).json({ error: "REST API unable to process your request." }));
+      .then((response) => {
+        if (response.ok) {
+          res.status(200).json({ success: "Order sucessfully submitted." });
+        } else {
+          return res.status(400).json({ error: "Order was not submitted." });
+        }
+      })
+      .catch((error) => {
+        res
+          .status(500)
+          .json({ error: "REST API unable to process your request." });
+      });
   } else {
     res.setHeader("Allow", ["POST"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
@@ -38,7 +45,7 @@ const isBodyValid = async (requestBody) => {
     !body.ExternalCustomerReference.startsWith("_") ||
     !body.DeliveryDetails ||
     !body.BillingDetails ||
-    !body.PaymentMethod
+    !body.PaymentDetails
   ) {
     return false;
   } else return true;
