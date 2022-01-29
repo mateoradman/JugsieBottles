858afadc1e848482e8d9d34b79733f31e4d70434
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-01-29 18:49:53 +0100

    add swagger scheme overview urls (drf_spectacular)

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+schema_urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(), name='swagger-ui'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/product/', include('product.urls')),
     path('api/v1/shop/', include('shop.urls')),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-]
+] + schema_urlpatterns
