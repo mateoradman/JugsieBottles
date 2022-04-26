a64b5f79b8af94fa677363b02d00bc631dab8fc0
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-04-26 21:39:28 +0200

    Delete Django api files

--- api/faq/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import FAQSection, QuestionAndAnswer
-
-admin.site.register(FAQSection)
-admin.site.register(QuestionAndAnswer)
--- api/product/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import BottleDetail, BottleList
-
-urlpatterns = [
-    path('bottles/<int:pk>', BottleDetail.as_view(), name='bottle'),
-    path('bottles/', BottleList.as_view(), name='all_bottles'),
-]
