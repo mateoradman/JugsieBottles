6a6dcf276fb5d418b9fb22a60ec9aa32ee3a082f
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-05-11 22:40:47 +0200

    work in progress: implement both bank transfer and credit card payment options

--- app/components/checkout/CheckoutForm.js
+++ app/components/checkout/CheckoutForm.js
@@ -1,28 +1,37 @@
-import {useTranslation} from "next-i18next";
-import React, {useState} from "react";
-import {v4 as uuidv4} from 'uuid';
-import {useCartItems} from "../../context/Context";
-import {DEFAULT_CURRENCY} from "../../utils/constants";
-import {classNames} from "../../utils/general";
-import {OrderSummary} from "./OrderSummary";
-import PaymentForm from "./PaymentForm";
+import { useTranslation } from "next-i18next";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { useCartItems } from "../../context/Context";
+import { DEFAULT_CURRENCY } from "../../utils/constants";
+import { classNames } from "../../utils/general";
+import { InfoAlert, NeutralAlert } from "../alerts";
+import BankTransferForm from "./BankTransferForm";
+import CreditCardForm from "./CreditCardForm";
+import { OrderSummary } from "./OrderSummary";
 import PersonalDetailsForm from "./PersonalDetailsForm";
 
 export default function CheckoutForm() {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(2);
   const handleGoToNextStep = () => setStep((prevStep) => prevStep + 1);
   const handleGoToPreviousStep = () => {
     setStep((prevStep) => prevStep - 1);
     setFormData(defaultFormData);
   };
   const colourStep = (currentStep, threshold) => {
-    return classNames("step", currentStep >= threshold ? "step-success" : "");
+    return classNames("step", currentStep >= threshold ? "step-primary" : "");
+  }
+
+  // payment by card or bank transfer possible. Default is bank transfer
+  const [paymentByCard, setPaymentByCard] = useState(false);
+  const handlePaymentMethod = (event) => {
+    setPaymentByCard(event.target.value);
   }
 
   const defaultFormData = {
-    Currency: DEFAULT_CURRENCY,
-    Source: "Website",
-    OrderUUID: uuidv4(),
+    currency: DEFAULT_CURRENCY,
+    orderUUID: uuidv4(),
+    coupon: null,
+    paymentOption: paymentByCard ? "CreditCard" : "BankTransfer",
   };
   const [formData, setFormData] = useState(defaultFormData);
   const updateFormData = (newFormData) => {
@@ -39,21 +48,28 @@ export default function CheckoutForm() {
       case 1:
         return (
           <PersonalDetailsForm
-            updateFormData={ updateFormData }
-            handleGoToNextStep={ handleGoToNextStep }
+            updateFormData={updateFormData}
+            handleGoToNextStep={handleGoToNextStep}
           />
         );
       case 2:
         return (
-          <PaymentForm
-            formData={ formData }
-            updateFormData={ updateFormData }
-            handleGoToNextStep={ handleGoToNextStep }
-            handleGoToPreviousStep={ handleGoToPreviousStep }
-            step={ step }
-            cartItemsArray={ cartItemsArray }
-            setCartItemsArray={ setCartItemsArray }
-          />
+          <>
+            {paymentByCard ? (
+              <CreditCardForm
+                formData={formData}
+                updateFormData={updateFormData}
+                handleGoToNextStep={handleGoToNextStep}
+                handleGoToPreviousStep={handleGoToPreviousStep}
+                step={step}
+                cartItemsArray={cartItemsArray}
+                setCartItemsArray={setCartItemsArray}
+              />
+            ) : (
+              <div>AOSkfosdkfsdop</div>
+              // <BankTransferForm></BankTransferForm>
+            )}
+          </>
         );
       default:
         return null;
@@ -65,18 +81,23 @@ export default function CheckoutForm() {
   return (
     <div className="relative flex flex-col items-center justify-center overflow-hidden">
       <ul className="w-full steps">
-        <li className={ colourStep(step, 1) }>
-          <span className="label-text">{ t('personalDetails') }</span>
+        <li className={colourStep(step, 1)}>
+          <span className="label-text">{t('personalDetails')}</span>
         </li>
-        <li className={ colourStep(step, 2) }>
-          <span className="label-text">{ t('payment') }</span>
+        <li className={colourStep(step, 2)}>
+          <span className="label-text">{t('payment')}</span>
         </li>
         <li className="step">
-          <span className="label-text">{ t('orderComplete') }</span>
+          <span className="label-text">{t('orderComplete')}</span>
         </li>
       </ul>
-      <OrderSummary cartItemsArray={ cartItemsArray } />
-      { componentToRender() }
+      {step === 2 &&
+      (<div className="mx-6 mt-6 flex font-medium text-sm">
+        <InfoAlert message={t('bankTransferAlert')}/>
+      </div>)
+      }
+      <OrderSummary cartItemsArray={cartItemsArray} />
+      {componentToRender()}
     </div>
   );
 }
