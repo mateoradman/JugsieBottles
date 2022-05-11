66c993becf824b9711fcf7b543faaa7255ff8323
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-05-11 22:41:01 +0200

    Add new alert

--- app/components/alerts/index.js
+++ app/components/alerts/index.js
@@ -1,7 +1,8 @@
+import { ExclamationIcon } from "@heroicons/react/outline"
 
 export const SuccessAlert = ({ message }) => {
     return (
-        <div className="alert alert-success shadow-lg">
+        <div className="alert alert-success shadow-lg select-none">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={"2"} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span>{message}</span>
@@ -12,9 +13,31 @@ export const SuccessAlert = ({ message }) => {
 
 export const ErrorAlert = ({ message }) => {
     return (
-        <div className="alert alert-error shadow-lg">
+        <div className="alert alert-error shadow-lg select-none">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={"2"} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span>{message}</span>
+            </div>
+        </div>
+    )
+}
+
+export const NeutralAlert = ({ message }) => {
+    return (
+        <div className="alert shadow-lg select-none">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <span>{message}</span>
+            </div>
+        </div>
+    )
+}
+
+export const InfoAlert = ({ message }) => {
+    return (
+        <div className="alert alert-info shadow-lg select-none">
+            <div>
+                <ExclamationIcon className="w-5 mr-2" />
                 <span>{message}</span>
             </div>
         </div>
