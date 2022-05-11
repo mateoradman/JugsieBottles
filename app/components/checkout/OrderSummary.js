b7ffd2b8f95820bda70e3d226e22f6e0e9e4c6b1
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-05-11 22:41:49 +0200

    change the order summary appearance

--- app/components/checkout/OrderSummary.js
+++ app/components/checkout/OrderSummary.js
@@ -11,26 +11,23 @@ export const OrderSummary = (props) => {
         <div className="mt-6 px-5 sm:px-0 sm:mx-auto sm:w-full sm:max-w-xl">
             <div className="flex flex-col space-y-4 w-full p-2 mx-auto bg-white rounded-2xl">
                 <Disclosure defaultOpen>
-                    { ({ open }) => (
+                    {({ open }) => (
                         <>
                             <Disclosure.Button
                                 className={
-                                    `flex justify-between self-center px-6 sm:px-12 py-2 text-lg 
-                                    sm:text-xl font-medium text-left text-green-900 bg-green-100 rounded-lg 
-                                  hover:bg-green-200 focus:outline-none focus-visible:ring focus-visible:ring-green-500 
-                                    focus-visible:ring-opacity-75`
+                                    `flex justify-between self-center btn btn-sm btn-primary btn-outline hover:text-accent`
                                 }
                             >
-                                <span>{ t('orderSummary') }</span>
+                                <span>{t('orderSummary')}</span>
                                 <ChevronUpIcon
-                                    className={ classNames(
-                                        "text-purple-900 w-5 h-5 ml-5 self-center",
+                                    className={classNames(
+                                        "w-5 h-5 ml-3 self-center",
                                         open ? "transform rotate-180" : ""
-                                    ) }
+                                    )}
                                 />
                             </Disclosure.Button>
                             <Transition
-                                show={ open }
+                                show={open}
                                 enter="transition duration-100 ease-out"
                                 enterFrom="transform scale-95 opacity-0"
                                 enterTo="transform scale-100 opacity-100"
@@ -40,16 +37,16 @@ export const OrderSummary = (props) => {
                             >
                                 <Disclosure.Panel static>
                                     <div className="flex flex-col space-y-5 text-sm card bordered rounded-lg shadow-md p-4">
-                                        <CartProducts cartItemsArray={ props.cartItemsArray } />
+                                        <CartProducts cartItemsArray={props.cartItemsArray} />
                                         <span className="self-end sm:text-lg font-extrabold">
-                                            { t('totalPrice') }:{ " " }
-                                            { getFormattedPrice(getCartTotalPrice(props.cartItemsArray)) }
+                                            {t('totalPrice')}:{" "}
+                                            {getFormattedPrice(getCartTotalPrice(props.cartItemsArray))}
                                         </span>
                                     </div>
                                 </Disclosure.Panel>
                             </Transition>
                         </>
-                    ) }
+                    )}
                 </Disclosure>
             </div>
         </div>
