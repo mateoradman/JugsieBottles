bed8ee5fe4acfda3c5ca861255d460f247abb268
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-06-16 16:19:02 +0200

    Remove 360 card from the index page.

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,4 +1,4 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import BottleDisplay from "../components/homePage/bottlePhotos/BottleDisplay";
 import ThreeSixtyCard from "../components/homePage/cards/360Card";
 import HomePageCard from "../components/homePage/cards/HomePageCard";
@@ -9,24 +9,23 @@ export default function Home() {
   return (
     <div
       className="relative min-h-screen overflow-hidden">
-      <Hero />
-      <MarqueeRunningText />
-      <BottleDisplay />
+      <Hero/>
+      <MarqueeRunningText/>
+      <BottleDisplay/>
 
       <div
         className="pb-16 px-8 pt-8 grid grid-cols-1 gap-y-10 md:px-5 md:pt-2 md:grid-cols-2 md:gap-x-16">
         <HomePageCard
-          cardImageSrc={ "/ContentPhotos/IMG_9820.JPG" }
-          cardTitle={ "Some Title" }
-          cardBody={ "lorem ipsum something else here" }
+          cardImageSrc={"/ContentPhotos/IMG_9820.JPG"}
+          cardTitle={"Some Title"}
+          cardBody={"lorem ipsum something else here"}
         />
-        <ThreeSixtyCard />
       </div>
     </div>
   )
 }
 
-export const getStaticProps = async ({ locale }) => ({
+export const getStaticProps = async ({locale}) => ({
   props: {
     ...await serverSideTranslations(locale, ['common', 'index']),
   },
