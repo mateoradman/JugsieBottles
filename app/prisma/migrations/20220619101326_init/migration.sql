8d1de6941dfc5f7ab19c8e18144580599bf54e1d
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-06-19 12:14:29 +0200

    Add initial migration for the database

--- app/prisma/migrations/20220619101326_init/migration.sql
+++ app/prisma/migrations/20220619101326_init/migration.sql
@@ -1,10 +1,19 @@
+-- CreateEnum
+CREATE TYPE "PersonalizationIcon" AS ENUM ('Aeroplane', 'Butterfly', 'Camera', 'Clover', 'Crown', 'Dumbbell', 'Flower', 'Heart', 'Lipstick', 'MusicalNote', 'Paw', 'Smile', 'Star', 'Tooth');
+
+-- CreateEnum
+CREATE TYPE "Locale" AS ENUM ('en', 'hr');
+
+-- CreateEnum
+CREATE TYPE "PaymentOption" AS ENUM ('CreditCard', 'BankTransfer');
+
 -- CreateTable
 CREATE TABLE "Bottle" (
     "id" SERIAL NOT NULL,
     "colour" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
-    "stock" INTEGER NOT NULL,
+    "stock" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "Bottle_pkey" PRIMARY KEY ("id")
 );
@@ -23,11 +32,14 @@ CREATE TABLE "Customer" (
 -- CreateTable
 CREATE TABLE "Order" (
     "id" SERIAL NOT NULL,
+    "orderUUID" TEXT NOT NULL,
     "customerId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "couponId" INTEGER NOT NULL,
-    "currency" TEXT NOT NULL,
+    "couponId" INTEGER,
+    "currency" TEXT NOT NULL DEFAULT E'HRK',
+    "paymentOption" "PaymentOption" NOT NULL DEFAULT E'BankTransfer',
+    "locale" "Locale" NOT NULL,
 
     CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
@@ -37,8 +49,8 @@ CREATE TABLE "OrderItem" (
     "id" SERIAL NOT NULL,
     "bottleId" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
-    "personalizationText" TEXT NOT NULL,
-    "personalizationIcon" TEXT NOT NULL,
+    "personalizationText" TEXT NOT NULL DEFAULT E'',
+    "personalizationIcon" "PersonalizationIcon",
     "price" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -67,7 +79,6 @@ CREATE TABLE "Address" (
     "customerId" INTEGER NOT NULL,
     "street" TEXT NOT NULL,
     "city" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
     "zip" TEXT NOT NULL,
     "country" TEXT NOT NULL,
     "isShipping" BOOLEAN NOT NULL,
@@ -77,6 +88,30 @@ CREATE TABLE "Address" (
     CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Faq" (
+    "id" SERIAL NOT NULL,
+    "question" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "section" TEXT NOT NULL,
+    "locale" "Locale" NOT NULL,
+    "icon" TEXT NOT NULL,
+
+    CONSTRAINT "Faq_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "phone" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "locale" "Locale" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Bottle_colour_key" ON "Bottle"("colour");
 
@@ -84,13 +119,19 @@ CREATE UNIQUE INDEX "Bottle_colour_key" ON "Bottle"("colour");
 CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Order_orderUUID_key" ON "Order"("orderUUID");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Coupon_code_key" ON "Coupon"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Faq_question_key" ON "Faq"("question");
 
 -- AddForeignKey
 ALTER TABLE "Order" ADD CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_couponId_fkey" FOREIGN KEY ("couponId") REFERENCES "Coupon"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Order" ADD CONSTRAINT "Order_couponId_fkey" FOREIGN KEY ("couponId") REFERENCES "Coupon"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_bottleId_fkey" FOREIGN KEY ("bottleId") REFERENCES "Bottle"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -100,3 +141,6 @@ ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("or
 
 -- AddForeignKey
 ALTER TABLE "Address" ADD CONSTRAINT "Address_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
