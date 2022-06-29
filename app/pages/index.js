96ed0a0d6a8b9764b6583d83a63dd158b798ccea
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-06-29 19:34:06 +0200

    Add skateboard photo to public/Images and change the path in the card pointing to this skateboard

--- app/pages/index.js
+++ app/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <div
         className="pb-16 px-8 pt-8 grid grid-cols-1 gap-y-10 md:px-5 md:pt-2 md:grid-cols-2 md:gap-x-16">
         <HomePageCard
-          cardImageSrc={"/ContentPhotos/IMG_9820.JPG"}
+          cardImageSrc={"/Images/skateboard.jpg"}
           cardTitle={"Some Title"}
           cardBody={"lorem ipsum something else here"}
         />
