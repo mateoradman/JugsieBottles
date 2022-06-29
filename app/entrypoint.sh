9726fe24ae5818e52e4215f31bb9e513f3f9c1b1
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-06-29 19:27:52 +0200

    Delete unused scripts

--- app/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Waiting for API to respond..."
-while ! nc -z "$API_HOSTNAME" "$API_PORT"; do
-  sleep 1
-done
-echo "API reachable!"
-exec "$@"
--- scripts/prepopulate_database.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker exec jugsie_backend python3 manage.py makemigrations
-docker exec jugsie_backend python3 manage.py migrate
-docker exec jugsie_backend python3 manage.py pre_populate_users
-docker exec jugsie_backend python3 manage.py pre_populate_faqs
-docker exec jugsie_backend python3 manage.py pre_populate_bottles
