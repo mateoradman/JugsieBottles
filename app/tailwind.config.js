6d2f5584965246048ab557c3d28f9cc221e8e02f
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-02 09:22:10 +0200

    configure eslint and remove unused keys from tailwind config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}',
     './utils/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
