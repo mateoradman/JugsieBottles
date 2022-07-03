141aa5b33c545af00779927da309501bf182067b
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-03 13:17:31 +0200

    center all icons in incentives component

--- app/components/shop/incentives/index.js
+++ app/components/shop/incentives/index.js
@@ -3,14 +3,13 @@ import { useTranslation } from 'next-i18next'
 
 
 export default function Incentives() {
-  const {t} = useTranslation('shop')
+  const { t } = useTranslation('shop')
   return (
     <div className="bg-white">
-      <h2 className="sr-only">Our perks</h2>
       <div className="max-w-7xl mx-auto divide-y divide-gray-200 lg:py-8 lg:flex lg:justify-center lg:divide-y-0 lg:divide-x">
         <div className="py-8 lg:py-0 lg:w-1/3 lg:flex-none">
           <div className="max-w-xs mx-auto px-4 flex items-center lg:max-w-none lg:px-8">
-            <CalendarIcon height={30} className={"mb-3"}/>
+            <CalendarIcon height={30} />
             <div className="ml-4 flex-auto flex flex-col-reverse">
               <p className="text-2xs text-gray-500"><sup>1</sup>{t('incentiveDisclaimerOne')}</p>
               <h3 className="font-medium text-gray-900">{t('incentiveTitleOne')}<sup>1</sup></h3>
@@ -20,7 +19,7 @@ export default function Incentives() {
         </div>
         <div className="py-8 lg:py-0 lg:w-1/3 lg:flex-none">
           <div className="max-w-xs mx-auto px-4 flex items-center lg:max-w-none lg:px-8">
-            <RefreshIcon height={30} className={"mb-3"}/>
+            <RefreshIcon height={30} />
             <div className="ml-4 flex-auto flex flex-col-reverse">
               <p className="text-2xs text-gray-500"><sup>2</sup>{t('incentiveDisclaimerTwo')}</p>
               <h3 className="font-medium text-gray-900">{t('incentiveTitleTwo')}<sup>2</sup></h3>
@@ -30,7 +29,7 @@ export default function Incentives() {
         </div>
         <div className="py-8 lg:py-0 lg:w-1/3 lg:flex-none">
           <div className="max-w-xs mx-auto px-4 flex items-center lg:max-w-none lg:px-8">
-            <TruckIcon height={30} className={"mb-3"}/>
+            <TruckIcon height={30} />
             <div className="ml-4 flex-auto flex flex-col-reverse">
               <p className="text-2xs text-gray-500"><sup>3</sup>{t('incentiveDisclaimerThree')}</p>
               <h3 className="font-medium text-gray-900">{t('incentiveTitleThree')}<sup>3</sup></h3>
