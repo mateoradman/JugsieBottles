38d1c949b92e94155f6f8279812134361ad9d848
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-03 17:50:18 +0200

    seed translated faqs

--- app/prisma/seed.js
+++ app/prisma/seed.js
@@ -1,4 +1,4 @@
-const {PrismaClient} = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
@@ -39,7 +39,7 @@ const bottles = [
     price: 130,
   },
 ]
-const faq_en = [
+const faqs = [
   {
     "question": "What are Jugsie bottles made from?",
     "answer": "Jugsie bottles are made from 18/8 stainless steel (18% chromium and 8% nickel) on both the inside and outside. The lid is made from stainless steel, plastic and a silicon o-ring.",
@@ -249,20 +249,231 @@ const faq_en = [
     "section": "Returns",
     "locale": "en",
     "icon": "ReceiptRefundIcon"
+  },
+  {
+    "question": "Od čega je izrađena Jugsie boca?",
+    "answer": "Jugsie boca izrađena je od nehrđajućeg čelika 18/8 (18% kroma i 8% nikla) s unutarnje i vanjske strane. Poklopac je izrađen od nehrđajućeg čelika, plastike i silikonskog o-prstena.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Gdje se izrađuje Jugsie boca?",
+    "answer": "Jugsie je tvrtka sa sjedištem u Hrvatskoj, a naši proizvodi se proizvode u Kini.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Koja je veličina i težina bočica Jugsie?",
+    "answer": "Boce od 500 ml: visina 26 cm, širina 7 cm i težina 0,38 kg.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Jesu li Jugsie bočice izdržljive?",
+    "answer": "Jugsie boce izrađene su od visokokvalitetnog nehrđajućeg čelika s unutarnje i vanjske strane što ih čini izuzetno izdržljivima. Ipak, savjetujemo vam da budete oprezni s bocom jer naše jamstvo ne pokriva slučajne štete.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Jesu li Jugsie boce nepropusne?",
+    "answer": "Da, Jugsie je dizajniran tako da ne propušta vodu. Naše boce imaju nepropusni poklopac sa silikonskim o-prstenom koji sprječava curenje i pomaže u održavanju temperature pića.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Održavaju li Jugsie bočice pića vrućima?",
+    "answer": "Da, Jugsie boce mogu držati pića vrućim do 12 sati. Kako biste iz svoje Jugsie boce izvukli najbolje, preporučujemo da bocu najprije napunite kipućom vodom da biste je zagrijali, a zatim ispraznite vodu i napunite odabrani topli napitak. Tako će vaš topli napitak dulje ostati vruć. Ovisno o tome koliko se puta bočica otvori i o temperaturi pića, rezultat od 12 sati može varirati.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Odgovara li veličina Jugsie bočice držaču za čaše u automobilu?",
+    "answer": "Jugsie boce su standardne veličine te odgovaraju velikoj većini (standardne veličine) držača za automobilske čaše. Međutim, preporučujemo da prvo provjerite veličinu držača za čaše i veličinu boce.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Mogu li svoju Jugsie bocu ponijeti kroz osiguranje u zračnoj luci?",
+    "answer": "Da, trebali biste moći provesti svoju bocu Jugsie kroz osiguranje zračne luke sve dok je boca prazna. Međutim, neke zračne luke dopuštaju tekućinu, dok neke čak ne dopuštaju prazne boce - zato je bolje provjeriti u zračnoj luci prije leta.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Jesu li bočice Jugsie bez BPA?",
+    "answer": "•	Da - Jugsie bočice i poklopci bez BPA.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Kako mogu očistiti Jugsie bocu?",
+    "answer": "Preporučujemo čišćenje boce nakon svake upotrebe toplom vodom (po potrebi upotrijebite sapun), no bocu nemojte ostavljati da se natopi. Radije ju očistite i ostavite poklopac otvoren da se osuši kako biste spriječili pojavljivanje neželjenog mirisa ili okusa. Kad ne upotrebljavate bocu, ostavite poklopac otvorenim.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Mogu li koristiti perilicu posuđa?",
+    "answer": "Ne, bočice Jugsie nisu prikladne za perilice posuđa.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Mogu li svoju Jugsie bočicu staviti u hladnjak ili zamrzivač?",
+    "answer": "Ne bismo savjetovali stavljanje boca Jugsie u hladnjak ili zamrzivač. To je zato što to neće imati utjecaja na bocu zbog dvostruke stijenke vakuumske izolacije. Ova tehnologija znači da vanjska temperatura na bocu ne utječe, tako da hladnjak ili zamrzivač neće moći rashladiti sadržaj unutra. Također je moguće da dođe do malih puknuća i slomi vakuumsku brtvu ako se ostavi u zamrzivaču. Za najbolje rezultate hladne vode preporučujemo upotrebu vrlo hladne vode iz hladnjaka ili slavine i punjenje Jugsie boce kockicama leda.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Mogu li svoju Jugsie bočicu staviti u mikrovalnu?",
+    "answer": "Kao što već vjerojatno znate, metali i mikrovalna ne idu zajedno. Stoga, nemojte stavljati bočicu Jugsie u mikrovalnu pećnicu jer za nju nije prikladna.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Osim vode, koje tekućine mogu koristiti u boci Jugsie?",
+    "answer": "Jugsie boce su prvenstveno poznate i dizajnirane za ljubitelje vode. Međutim, sasvim je u redu koristiti ga za bilo koje drugo piće, bilo alkoholno ili gazirano piće, juhu, mlijeko ili kavu. Pazite da ga nakon toga temeljito očistite toplom vodom sa sapunom kako bi tvoja Jugsie boca bila spremna za ponovno korištenje.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Mogu li personalizirati svoju Jugsie bočicu?",
+    "answer": "Da - za sada nudimo graviranje Vaše Jugsie boce. Uskoro ćemo Vam ponuditi još nekoliko dodataka koji će omogućiti još veću personalizaciju.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Mogu li ugravirati simbole na svoju bočicu Jugsie?",
+    "answer": "Da! Znamo koliko ljudi vole simbole, zbog čega vam dopuštamo da odaberete između nekoliko simbola koji ćete ugravirati na svoju Jugsie bočicu kako biste je učinili što personaliziranijom! Naravno, možete odabrati samo slova ili simbole, ili oboje - na vama je. Maximalan broj znakova je 10",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Mogu li kupiti zamjenske čepove?",
+    "answer": "Za sada nudimo samo čepove koji dolaze uz odabranu Jugsie bocu. Uskoro ćete imati više mogućnosti za odabir.",
+    "section": "Generalno",
+    "locale": "hr",
+    "icon": "InformationCircleIcon"
+  },
+  {
+    "question": "Gdje je moja e-pošta s potvrdom?",
+    "answer": "Trebali biste primiti e-poštu s potvrdom u roku od nekoliko minuta nakon završetka kupnje.",
+    "section": "Narudžbe i dostava",
+    "locale": "hr",
+    "icon": "TruckIcon"
+  },
+  {
+    "question": "Nisam primio e-poštu s potvrdom",
+    "answer": "Molimo vas, provjerite svoju neželjenu poštu jer se e-pošta ponekad tamo izgubi. Ako još uvijek nema tragova, kontaktirajte našu službu za korisnike.",
+    "section": "Narudžbe i dostava",
+    "locale": "hr",
+    "icon": "TruckIcon"
+  },
+  {
+    "question": "Mogu li pratiti svoju narudžbu?",
+    "answer": "Da, nakon što je Vaša Jugie boca poslana, primit ćete e-poštu s informacijama o praćenju koja će omogućiti praćenje Vaše narudžbe od našeg skladišta do Vaših vrata.",
+    "section": "Narudžbe i dostava",
+    "locale": "hr",
+    "icon": "TruckIcon"
+  },
+  {
+    "question": "Primio sam pogrešnu narudžbu.",
+    "answer": "Žao nam je što ste primili pogrešnu narudžbu! Molimo kontaktirajte našu službu za korisnike na info@jugsie.com i uskoro ćemo vas kontaktirati.",
+    "section": "Narudžbe i dostava",
+    "locale": "hr",
+    "icon": "TruckIcon"
+  },
+  {
+    "question": "Koliko košta poštarina i koliko traje dostava?",
+    "answer": "Za narudžbe veće od 100 HRK nudimo besplatnu dostavu s isporukom od 7-10 radnih dana. Za narudžbe ispod 100HRK, dostava košta 20HRK.",
+    "section": "Narudžbe i dostava",
+    "locale": "hr",
+    "icon": "TruckIcon"
+  },
+  {
+    "question": "Kako isporučujete boce Jugsie?",
+    "answer": "Jugsie boce isporučujemo u Hrvatsku s firmom GLS. Trenutno ne dostavljamo izvan Hrvatske, ali uskoro to planiramo promijeniti. ",
+    "section": "Narudžbe i dostava",
+    "locale": "hr",
+    "icon": "TruckIcon"
+  },
+  {
+    "question": "Dostavljate li Jugsie izvan Hrvatske?",
+    "answer": "Nažalost, ne. Za sada Jugsie boce isporučujemo samo unutar Hrvatske, ali planiramo proširiti dostavu na Europu u kratkom periodu.",
+    "section": "Narudžbe i dostava",
+    "locale": "hr",
+    "icon": "TruckIcon"
+  },
+  {
+    "question": "Mogu li vratiti narudžbu?",
+    "answer": "Da, svoju bočicu Jugsie možete vratiti u roku od 30 dana od kupnje.* Sve Jugsie boce pokrivene su proizvodnim nedostacima godinu dana nakon kupnje. Napominjemo da nam je za bilo kakav povratak / zamjenu potreban dokaz o kupnji.* Personalizirane (gravirane) i korištene bočice Jugsie ne mogu se vratiti zbog nemogućnosti ponovne prodaje. \\tSve dok Vaša Jugsie bočica nije korištena ili personalizirana, izdat ćemo puni povrat novca nakon što je predmet primljen u naše skladište. Napominjemo da troškove povrata mora pokriti kupac. \\tSve isporuke dolaze s naljepnicom s povratnom adresom. Jugsie se ne može smatrati odgovornim za bilo kakve povrate koji se izgube na pošti, zbog čega preporučujemo upotrebu usluge s potpisom kada nam šaljete povrat. \\tMolimo osigurajte da svi povrati stignu do nas u prodajnom stanju. Na primjer, nemojte lijepiti naljepnice na ambalažu.",
+    "section": "Povrat",
+    "locale": "hr",
+    "icon": "ReceiptRefundIcon"
+  },
+  {
+    "question": "Mogu li otkazati narudžbu?",
+    "answer": "Nažalost, ne možemo jamčiti mogucnost otkazivanja Vaše narudžbe zbog toga što se velika većina narudžbi odmah priprema za isporuku, ali kontaktirajte nas na info@jugsie.com čim saznate da želite otkazati svoju narudžbu pa će možda biti prilike otkazati je. U slučaju da je moguće otkazati Vašu narudžbu, primit ćete puni povrat novca. Ukoliko to nije moguće, nekorištenu i nepersonaliziranu bocu možete vratiti kroz 30 dana. ",
+    "section": "Returns",
+    "locale": "hr",
+    "icon": "ReceiptRefundIcon"
+  },
+  {
+    "question": "Moja je narudžba stigla oštećena.",
+    "answer": "Žao nam je što je Vaša narudžba stigla oštećena. Ako je proizvod oštećen u prijevozu, tada ćemo ga zamijeniti besplatno. Molimo vas da nam ne šaljete neispravne proizvode, već nas kontaktirajte što je prije moguće  na info@jugsie.com.",
+    "section": "Povrat",
+    "locale": "hr",
+    "icon": "ReceiptRefundIcon"
+  },
+  {
+    "question": "Što pokriva jamstvo?",
+    "answer": "Jugsie boce dizajnirane su za upotrebu tijekom cijelog života. Međutim, ako nešto pođe po zlu, naše jamstvo pokriva Vas protiv proizvodnih nedostataka godinu dana od dana kupnje. Sve što trebate je dokaz o kupnji koji može potvrditi datum kupnje i osigurati autentičnost proizvoda. Kako Jugsie boce imaju za cilj smanjenje otpada, s ponosom možemo reći da nudimo odgovarajuće zamjene neispravnih dijelova Jugsie boca, umjesto da cijelu bocu zamijenimo novom. Ako ista boja nije dostupna, dat ćemo sve od sebe da Vam omogućimo sto sličniji dio originalnom. Znakovi korištenja, poput ogrebotina i udubljenja, nije pokriveno jamstvom. Također, šteta nastala nepravilnim čišćenjem ili preinakama od treće strane također nije uključena u naše jamstvo. ",
+    "section": "Povrat",
+    "locale": "hr",
+    "icon": "ReceiptRefundIcon"
+  },
+  {
+
+    "question": "Zahtjev pod jamstvom",
+    "answer": "Ako mislite da je Vaš predmet neispravan, prvo nas kontaktirajte i rado ćemo ga pogledati. Opišite što nije u redu i priložite fotografiju svog predmeta. Neispravni proizvodi ispunjavaju uvjete za povrat novca do trideset dana nakon kupnje. Nakon toga zamjenu možemo ponuditi samo uz jamstvo. Molimo Vas imajte na umu da se jamstvo ne ponovno pokreće kada primite zamjensku bocu.",
+    "section": "Povrat",
+    "locale": "hr",
+    "icon": "ReceiptRefundIcon"
+  },
+  {
+    "question": "Proizvodi koji su stigli oštećeni",
+    "answer": "Ako narudžba stigne oštećena, nemojte je koristiti jer će poništiti jamstvo. Molimo kontaktirajte nas sa slikama neispravne boce i rado ćemo je zamijeniti.",
+    "section": "Povrat",
+    "locale": "hr",
+    "icon": "ReceiptRefundIcon"
   }
 ]
 
 async function main() {
   for (const bottle of bottles) {
     await prisma.bottle.upsert({
-      where: {colour: bottle.colour},
+      where: { colour: bottle.colour },
       update: {},
       create: bottle,
     })
   }
-  for (const faq of faq_en) {
+  for (const faq of faqs) {
     await prisma.faq.upsert({
-      where: {question: faq.question},
+      where: { question: faq.question },
       update: {},
       create: faq,
     })
