6456a49b7e17e3856b8c102be85ac13c6a6e0876
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-16 22:55:48 +0200

    use nanoid instead of ID function

--- app/components/cart/CartProducts.js
+++ app/components/cart/CartProducts.js
@@ -1,7 +1,7 @@
-import CartProduct from "./CartProduct";
-import { useCartItems } from "../../context/Context";
-import { ID } from "../../utils/general";
+import { nanoid } from "nanoid";
 import { useTranslation } from "next-i18next";
+import { useCartItems } from "../../context/Context";
+import CartProduct from "./CartProduct";
 
 const CartProducts = () => {
   const { cartItemsArray, setCartItemsArray } = useCartItems();
@@ -12,20 +12,20 @@ const CartProducts = () => {
 
   return (
     <div className="mt-8">
-      { cartItemsArray.length > 0 ?
+      {cartItemsArray.length > 0 ?
         <div className="flow-root">
           <ul role="list"
             className="-my-6 divide-y divide-gray-200">
-            { cartItemsArray.map((product) =>
-              <CartProduct key={ ID() } bottle={ product }
-                onRemove={ handleRemoveItemFromCart } />
-            ) }
+            {cartItemsArray.map((product) =>
+              <CartProduct key={nanoid()} bottle={product}
+                onRemove={handleRemoveItemFromCart} />
+            )}
           </ul>
         </div>
         :
         <p
           className="text-base text-gray-900">
-          { t("cartEmpty") }
+          {t("cartEmpty")}
         </p>
       }
 
