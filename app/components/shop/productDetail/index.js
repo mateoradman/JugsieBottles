7f1fb092eed5fb72031dee9095aa7192cceacf26
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-16 23:45:43 +0200

    Remove the word Jugsie Bottle after the colour name due to spacing on the phone display

--- app/components/shop/productDetail/index.js
+++ app/components/shop/productDetail/index.js
@@ -59,8 +59,8 @@ export const ProductCard = ({product}) => {
           className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
             <h1
-              className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
-              {`${selectedBottle.name} Jugsie Bottle (500ml)`}
+              className="text-xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+              {`${selectedBottle.name} (500ml)`}
             </h1>
             <p className="mt-4 text-gray-500 md:w-4/5">
               {t('bottleDescription')}
