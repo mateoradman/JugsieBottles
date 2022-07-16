f5f4b4242ddebd5140934e10f2908dfaa89fb633
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-16 22:00:52 +0200

    short circuit the function if method is not POST

--- app/pages/api/contacts.js
+++ app/pages/api/contacts.js
@@ -8,41 +8,39 @@ export async function contactHandler(req, res) {
     if (!req.headers.referer || !req.headers.referer.endsWith("/contact")) {
         return res.status(401).json({ error: "Unauthorized." });
     }
-
-    if (req.method === "POST") {
-        const { firstName, lastName, email, phone, message, locale } = JSON.parse(req.body);
-        const contact = await prisma.contact.create({
-            data: {
-                message: message,
-                locale: locale,
-                customer: {
-                    connectOrCreate: {
-                        create: {
-                            firstName: firstName,
-                            lastName: lastName,
-                            email: email,
-                            phone: phone
-                        },
-                        where: {
-                            email: email,
-                        }
-                    }
-                },
-            },
-        });
-        const msg = {
-            to: JUGSIE_EMAIL,
-            from: JUGSIE_EMAIL,
-            subject: "Nova poruka s web stranice",
-            text: `Imate novu poruku od ${firstName} ${lastName} (tel: ${phone}). Poruka: "${message}".`,
-        }
-        sgMail.send(msg)
-        // TODO send email to the customer as well using the template
-        return res.status(201).json({ contact });
-    } else {
+    if (req.method !== "POST") {
         // Method not supported
         return res.status(405).json({ error: "Method not supported." });
     }
+    const { firstName, lastName, email, phone, message, locale } = JSON.parse(req.body);
+    const contact = await prisma.contact.create({
+        data: {
+            message: message,
+            locale: locale,
+            customer: {
+                connectOrCreate: {
+                    create: {
+                        firstName: firstName,
+                        lastName: lastName,
+                        email: email,
+                        phone: phone
+                    },
+                    where: {
+                        email: email,
+                    }
+                }
+            },
+        },
+    });
+    const msg = {
+        to: "info@jugsie.com",
+        from: JUGSIE_EMAIL,
+        subject: "Nova poruka s web stranice",
+        text: `Imate novu poruku od ${firstName} ${lastName} (tel: ${phone}). Poruka: "${message}".`,
+    }
+    sgMail.send(msg);
+    // TODO send email to the customer as well using the template
+    return res.status(201).json({ contact });
 }
 
 export default withSentry(contactHandler);
