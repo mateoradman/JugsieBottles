5db742d0627727f31f014242cb30a53069872d13
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-17 18:44:16 +0200

    remove unused argument in useInput.js

--- app/hooks/useInput.js
+++ app/hooks/useInput.js
@@ -10,7 +10,7 @@ const useInput = (validateValue) => {
     setEnteredValue(event.target.value);
   }
 
-  const inputBlurHandler = (event) => {
+  const inputBlurHandler = () => {
     setIsTouched(true);
   }
 
