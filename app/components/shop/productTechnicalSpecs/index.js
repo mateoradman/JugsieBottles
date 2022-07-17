5db31978995b83e4f8c76b5c463bc4597e236806
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-17 13:50:10 +0200

    change spacing in product technical overview

--- app/components/shop/productTechnicalSpecs/index.js
+++ app/components/shop/productTechnicalSpecs/index.js
@@ -20,7 +20,7 @@ export default function TechnicalSpecs() {
   return (
     <div className="bg-white">
       <div
-        className="max-w-2xl mx-auto py-12 px-4 grid items-start grid-cols-1 gap-x-8 gap-y-16 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
+        className="max-w-2xl mx-auto py-16 px-4 grid items-start grid-cols-1 gap-x-8 gap-y-16 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
         <div>
           <h2
             className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
@@ -31,7 +31,7 @@ export default function TechnicalSpecs() {
           </p>
 
           <dl
-            className="mt-16 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 sm:gap-y-12 lg:gap-x-8">
+            className="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 sm:gap-y-12 lg:gap-x-8">
             {bottleFeatures.map((feature) => (
               <div key={feature.name} className="border-t border-gray-200 pt-2">
                 <dt className="font-medium text-gray-900">{feature.name}</dt>
