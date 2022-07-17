be080dade6ae5e722444a8c3ebb2e1ab5c6c8683
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-17 14:02:08 +0200

    Change colours of the contact form

--- app/components/contact/index.js
+++ app/components/contact/index.js
@@ -123,130 +123,34 @@ export const Contact = () => {
   return (
     <>
       <div className="bg-transparent">
-        <div className="max-w-7xl mx-auto py-8 px-4 sm:py-16 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
           <div className="relative bg-gray-100 shadow-xl">
             <h2 className="sr-only">{t('contactSupport', { ns: 'checkout' })}</h2>
             <div className="grid grid-cols-1 lg:grid-cols-3">
               {/* Contact information */}
-              <div className="relative overflow-hidden py-10 px-6 bg-indigo-700 sm:px-10 xl:p-12">
-                <div className="absolute inset-0 pointer-events-none sm:hidden" aria-hidden="true">
-                  <svg
-                    className="absolute inset-0 w-full h-full"
-                    width={343}
-                    height={388}
-                    viewBox="0 0 343 388"
-                    fill="none"
-                    preserveAspectRatio="xMidYMid slice"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z"
-                      fill="url(#linear1)"
-                      fillOpacity=".1"
-                    />
-                    <defs>
-                      <linearGradient
-                        id="linear1"
-                        x1="254.553"
-                        y1="107.554"
-                        x2="961.66"
-                        y2="814.66"
-                        gradientUnits="userSpaceOnUse"
-                      >
-                        <stop stopColor="#fff" />
-                        <stop offset={1} stopColor="#fff" stopOpacity={0} />
-                      </linearGradient>
-                    </defs>
-                  </svg>
-                </div>
-                <div
-                  className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none sm:block lg:hidden"
-                  aria-hidden="true"
-                >
-                  <svg
-                    className="absolute inset-0 w-full h-full"
-                    width={359}
-                    height={339}
-                    viewBox="0 0 359 339"
-                    fill="none"
-                    preserveAspectRatio="xMidYMid slice"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z"
-                      fill="url(#linear2)"
-                      fillOpacity=".1"
-                    />
-                    <defs>
-                      <linearGradient
-                        id="linear2"
-                        x1="192.553"
-                        y1="28.553"
-                        x2="899.66"
-                        y2="735.66"
-                        gradientUnits="userSpaceOnUse"
-                      >
-                        <stop stopColor="#fff" />
-                        <stop offset={1} stopColor="#fff" stopOpacity={0} />
-                      </linearGradient>
-                    </defs>
-                  </svg>
-                </div>
-                <div
-                  className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none lg:block"
-                  aria-hidden="true"
-                >
-                  <svg
-                    className="absolute inset-0 w-full h-full"
-                    width={160}
-                    height={678}
-                    viewBox="0 0 160 678"
-                    fill="none"
-                    preserveAspectRatio="xMidYMid slice"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z"
-                      fill="url(#linear3)"
-                      fillOpacity=".1"
-                    />
-                    <defs>
-                      <linearGradient
-                        id="linear3"
-                        x1="192.553"
-                        y1="325.553"
-                        x2="899.66"
-                        y2="1032.66"
-                        gradientUnits="userSpaceOnUse"
-                      >
-                        <stop stopColor="#fff" />
-                        <stop offset={1} stopColor="#fff" stopOpacity={0} />
-                      </linearGradient>
-                    </defs>
-                  </svg>
-                </div>
-                <h3 className="text-lg font-medium text-white">Jugsie Bottles</h3>
-                <p className="mt-6 text-base text-indigo-50 max-w-3xl">
+              <div className="relative overflow-hidden py-10 px-6 sm:px-10 xl:p-12 bg-gray-200">
+                <h3 className="text-lg font-medium text-gray-900">Jugsie Bottles</h3>
+                <p className="mt-6 text-base text-gray-900 max-w-3xl">
                   {t('contactText', { ns: 'common' })}
                 </p>
                 <dl className="mt-8 space-y-6">
                   <dt>
                     <span className="sr-only">{t('email')}</span>
                   </dt>
-                  <dd className="flex text-base text-indigo-50">
-                    <MailIcon className="flex-shrink-0 w-6 h-6 text-indigo-200" aria-hidden="true" />
+                  <dd className="flex text-base text-gray-900">
+                    <MailIcon className="flex-shrink-0 w-6 h-6 text-gray-900" aria-hidden="true" />
                     <span className="ml-3">info@jugsie.com</span>
                   </dd>
                 </dl>
                 <ul role="list" className="mt-8 flex space-x-12">
                   <li>
                     <Link href="https://www.instagram.com/jugsiebottles/">
-                      <a><FaInstagram style={{ ...iconFontSize, color: "lightcyan" }} /></a>
+                      <a><FaInstagram style={{ ...iconFontSize, color: "gray" }} /></a>
                     </Link>
                   </li>
                   <li>
                     <Link href="https://www.facebook.com/jugsiebottles/">
-                      <a><FaFacebook style={{ ...iconFontSize, color: "lightcyan" }} /></a>
+                      <a><FaFacebook style={{ ...iconFontSize, color: "gray" }} /></a>
                     </Link>
                   </li>
                 </ul>
