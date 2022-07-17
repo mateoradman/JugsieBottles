594e5da7bdf6442fe59d46ee46a534d22ff6fcfb
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-07-17 19:34:06 +0200

    slightly rework how product card is displayed on large screens.

--- app/components/shop/productDetail/index.js
+++ app/components/shop/productDetail/index.js
@@ -1,15 +1,15 @@
-import {useTranslation} from "next-i18next";
-import {useState} from "react";
-import {IoStar, IoStarHalf, IoStarOutline} from "react-icons/io5";
-import {useCartItems} from "../../../context/Context";
-import {bottleInformation, reviews} from "../../../utils/constants";
-import {classNames, getFormattedPrice} from "../../../utils/general";
+import { useTranslation } from "next-i18next";
+import { useState } from "react";
+import { IoStar, IoStarHalf, IoStarOutline } from "react-icons/io5";
+import { useCartItems } from "../../../context/Context";
+import { bottleInformation, reviews } from "../../../utils/constants";
+import { classNames, getFormattedPrice } from "../../../utils/general";
 import CartModal from "../../cart/CartModal";
 import ColourPicker from "./ColourPicker";
 import ProductImageGallery from "./ImageGallery";
-import {PersonalizationToggle} from "./Personalization";
+import { PersonalizationToggle } from "./Personalization";
 
-export const ProductCard = ({product}) => {
+export const ProductCard = ({ product }) => {
   const [isOpenCartModal, setOpenCartModal] = useState(false);
 
   const [selectedBottle, setSelectedBottle] = useState(bottleInformation[0]);
@@ -17,7 +17,7 @@ export const ProductCard = ({product}) => {
     setSelectedBottle(radioPickedBottle);
   }
 
-  const defaultPersonalization = {icon: "", text: ""};
+  const defaultPersonalization = { icon: "", text: "" };
   const [selectedPersonalization, setSelectedPersonalization] = useState(defaultPersonalization);
   const handleSelectedPersonalization = (personalization) => {
     setSelectedPersonalization(personalization);
@@ -28,7 +28,7 @@ export const ProductCard = ({product}) => {
     }
   }
 
-  const {cartItemsArray, setCartItemsArray} = useCartItems()
+  const { cartItemsArray, setCartItemsArray } = useCartItems()
   const getCompleteBottleObject = () => {
     return {
       ...selectedBottle,
@@ -43,62 +43,56 @@ export const ProductCard = ({product}) => {
     setOpenCartModal(true);
   }
 
-  const {t} = useTranslation('shop')
+  const { t } = useTranslation('shop')
 
   return (
     <div className="bg-white">
-      <div>
-        {/* Image gallery */}
-        <ProductImageGallery bottle={selectedBottle}
-                             personalization={selectedPersonalization}/>
+      <CartModal isOpen={isOpenCartModal} setOpen={setOpenCartModal}
+        bottle={getCompleteBottleObject()} />
+      <div
+        className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-2 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
+        <div className="lg:border-r lg:border-gray-200">
+          <ProductImageGallery bottle={selectedBottle}
+            personalization={selectedPersonalization} />
+        </div>
+        {/* Options */}
+        <div className="mt-4 lg:mt-0 lg:row-span-3">
+          <h1
+            className="text-xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+            {`${selectedBottle.name} (500ml)`}
+          </h1>
+          <p className="text-xl sm:text-3xl text-gray-900">
+            {getFormattedPrice(product.price)}
+          </p>
 
-        <CartModal isOpen={isOpenCartModal} setOpen={setOpenCartModal}
-                   bottle={getCompleteBottleObject()}/>
-        {/* Product info */}
-        <div
-          className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
-          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1
-              className="text-xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
-              {`${selectedBottle.name} (500ml)`}
-            </h1>
-          </div>
-
-          {/* Options */}
-          <div className="mt-4 lg:mt-0 lg:row-span-3">
-            <p className="text-3xl text-gray-900">
-              {getFormattedPrice(product.price)}
-            </p>
-
-            {/* Reviews */}
-            <div className="mt-6">
+          {/* Reviews */}
+          <div className="mt-4">
+            <div className="flex items-center">
               <div className="flex items-center">
-                <div className="flex items-center">
-                  {getStarsArray(reviews.average)}
-                </div>
-                <a href={reviews.href}
-                   target="_blank"
-                   rel="noreferrer"
-                   className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                  {reviews.totalCount} {t('reviews')}
-                </a>
+                {getStarsArray(reviews.average)}
               </div>
+              <a href={reviews.href}
+                target="_blank"
+                rel="noreferrer"
+                className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                {reviews.totalCount} {t('reviews')}
+              </a>
             </div>
-
-            {/* Colour and Personalization Form */}
-            <form onSubmit={handleSubmitBottle} className="mt-10">
-              <ColourPicker onBottleChange={handleSelectedBottle}/>
-              <PersonalizationToggle
-                defaultPersonalization={defaultPersonalization}
-                onPersonalizationChange={handleSelectedPersonalization}/>
-              <button
-                type="submit"
-                className={classNames("mt-10 w-full btn", selectedBottle.name === 'Black' ? "btn-disabled" : "btn-primary")}
-              >
-                {selectedBottle.name === 'Black' ? t('out-of-stock') : t('add-to-cart')}
-              </button>
-            </form>
           </div>
+
+          {/* Colour and Personalization Form */}
+          <form onSubmit={handleSubmitBottle} className="mt-8">
+            <ColourPicker onBottleChange={handleSelectedBottle} />
+            <PersonalizationToggle
+              defaultPersonalization={defaultPersonalization}
+              onPersonalizationChange={handleSelectedPersonalization} />
+            <button
+              type="submit"
+              className={classNames("mt-8 w-full btn", selectedBottle.name === 'Black' ? "btn-disabled" : "btn-primary")}
+            >
+              {selectedBottle.name === 'Black' ? t('out-of-stock') : t('add-to-cart')}
+            </button>
+          </form>
         </div>
       </div>
     </div>
@@ -108,7 +102,7 @@ export const ProductCard = ({product}) => {
 
 
 function getStarsArray(average) {
-  let FullStarsArray = Array.from({length: parseInt(average)}, (_v, i) => i);
+  let FullStarsArray = Array.from({ length: parseInt(average) }, (_v, i) => i);
 
   let returnArray = FullStarsArray.map((rating) => (
     <IoStar
